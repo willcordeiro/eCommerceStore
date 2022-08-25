d37d8003f35e3bfcb8169eef2922fb046323decd
willcordeiro <willcordeiro800@gmail.com>
2022-08-25 17:33:19 -0300

    Discont Section updated for mobile

--- src/components/discount/Dcard.jsx
+++ src/components/discount/Dcard.jsx
@@ -1,37 +1,40 @@
-import React from "react"
-import Slider from "react-slick"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
-import Ddata from "./Ddata"
-import "../newarrivals/style.css"
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Ddata from "./Ddata";
+import "../newarrivals/style.css";
+import "./Dcard.css";
 
 const Dcard = () => {
   const settings = {
     dots: false,
     infinite: true,
-    slidesToShow: 6,
+    slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: true,
-  }
+  };
   return (
     <>
       <Slider {...settings}>
         {Ddata.map((value, index) => {
           return (
             <>
-              <div className='box product' key={index}>
-                <div className='img'>
-                  <img src={value.cover} alt='' width='100%' />
+              <div className=" grid-container-dCord dCord-container">
+                <div className="product " key={index}>
+                  <div className="img">
+                    <img src={value.cover} alt="" className="img-center" />
+                  </div>
+                  <h4>{value.name}</h4>
+                  <span>{value.price}</span>
                 </div>
-                <h4>{value.name}</h4>
-                <span>{value.price}</span>
               </div>
             </>
-          )
+          );
         })}
       </Slider>
     </>
-  )
-}
+  );
+};
 
-export default Dcard
+export default Dcard;
