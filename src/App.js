bc6d0d5f08a7579a729c1a1174489980613a0ca2
willcordeiro <willcordeiro800@gmail.com>
2022-08-24 23:48:08 -0300

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/header/Header";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <Header />
+      <Router>
+        <Header />
+        {/* <Switch>
+          <Route path="/" exact></Route>
+  </Switch> */}
+      </Router>
     </>
   );
 }
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 function Navbar() {
+  const [MobileMenu, setMobileMenu] = useState(false);
   return (
     <>
       <header className="header">
@@ -12,11 +13,50 @@ function Navbar() {
             </h4>
           </div>
           <div className="navlink">
-            <ul className="nav">
+            <ul
+              className={
+                MobileMenu ? "nav-links-MobileMenu" : "link f_flex capitalize"
+              }
+              onClick={() => {
+                setMobileMenu(false);
+              }}
+            >
               <li>
-                <Link></Link>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/pages">pages</Link>
+              </li>
+              <li>
+                <Link to="/user">user account</Link>
+              </li>
+              <li>
+                <Link to="/user">user account</Link>
+              </li>
+              <li>
+                <Link to="/vendor">vendor account</Link>
+              </li>
+              <li>
+                <Link to="/track">track my order</Link>
+              </li>
+              <li>
+                <Link to="/contact">contact</Link>
               </li>
             </ul>
+
+            <button
+              className="toggle"
+              onClick={() => {
+                setMobileMenu(!MobileMenu);
+              }}
+            >
+              {" "}
+              {MobileMenu ? (
+                <i className="fas fa-times close home-bth"></i>
+              ) : (
+                <i className="fas fa-bars open"></i>
+              )}
+            </button>
           </div>
         </div>
       </header>
