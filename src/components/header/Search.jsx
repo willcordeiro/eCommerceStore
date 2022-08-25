b67e656829f64d3fa3d0ac58a80ceb07a6738a23
willcordeiro <willcordeiro800@gmail.com>
2022-08-25 00:14:35 -0300

    added seach.jsx css

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import logo from "../../assets/icons8-flower-playing-cards-48.png";
+import { Link } from "react-router-dom";
+
 function Search() {
+  window.addEventListener("scroll", function () {
+    const search = document.querySelector(".search");
+    search.classList.toggle("active", window.scrollY > 100);
+  });
+
   return (
     <>
-      <section className="seach">
+      <section className="search">
         <div className="container c_flex">
-          <div className="logo width">
+          <div className="logo width ">
             <img src={logo} alt="" />
           </div>
+
           <div className="search-box f_flex">
             <i className="fa fa-search"></i>
-            <input
-              type="text"
-              name=""
-              id=""
-              placeholder="Search and hit enter..."
-            />
+            <input type="text" placeholder="Search and hit enter..." />
             <span>All Category</span>
           </div>
+
           <div className="icon f_flex width">
-            <i className="fa-fa-user icons_circle"></i>
+            <i className="fa fa-user icon-circle"></i>
+            <div className="cart">
+              <Link to="/cart">
+                <i className="fa fa-shopping-bag icon-circle"></i>
+                <span>0</span>
+              </Link>
+            </div>
           </div>
         </div>
       </section>
